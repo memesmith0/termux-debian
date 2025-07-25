fc8eda0d07b9b5fc38ace5932d664ce3ae6a5aad
memesmith0 <meme.smith.wizard@gmail.com>
2025-07-25 06:19:03 -0700

    Update termux-debian.sh
    sodfijsodfijosfj

--- termux-debian.sh
+++ termux-debian.sh
@@ -8,7 +8,7 @@ echo ""
 echo "you will be prompted mulitple times"
 echo "just hit the enter key each time"
 echo "the install will start in 60 seconds"
-sleep 30
+sleep 60
 pkg update -y
 pkg upgrade -y
 pkg install -y proot
